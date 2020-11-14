b7977b2baab8b379ebd9563b96177b231adcc1e6
Ivar Troost <ivar@desagaz.com>
2020-11-14 10:32:30 +0100

    Implement marker service to retrieve buildings with geo-data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  sparqlEndpoints: {
+    uds:
+      'https://api.data.netwerkdigitaalerfgoed.nl/datasets/hetutrechtsarchief/UDS/services/UDS/sparql',
+  },
+
+  sparqlPrefixes: {
+    hua: `PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>`,
+  },
 };
 
 /*
