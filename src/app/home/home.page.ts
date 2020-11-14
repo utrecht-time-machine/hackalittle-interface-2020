3d055f649c597f97ba10340509e7abc9fb961c04
Simon <simon.dirks@hotmail.com>
2020-11-14 02:27:23 +0100

    Show/collapse more building info panel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('showingInfoAnimation', [
+      // ...
+      state('showingInfo', style({
+        height: '100vh'
+      })),
+      state('notShowingInfo', style({
+        height: '60vh'
+      })),
+
+      state('panoramaShowingInfo', style({
+        height: '0vh'
+      })),
+      state('panoramaNotShowingInfo', style({
+        height: '40vh'
+      })),
+
+      transition('showingInfo => notShowingInfo', [
+        animate('0.25s')
+      ]),
+      transition('notShowingInfo => showingInfo', [
+        animate('0.25s')
+      ]),
+      transition('panoramaShowingInfo => panoramaNotShowingInfo', [
+        animate('0.25s')
+      ]),
+      transition('panoramaNotShowingInfo => panoramaShowingInfo', [
+        animate('0.25s')
+      ]),
+    ]),]
 })
 export class HomePage {
+  showingMoreInfo = false;
 
   constructor() {}
 
+  showMoreInfo() {
+    this.showingMoreInfo = !this.showingMoreInfo;
+  }
 }
